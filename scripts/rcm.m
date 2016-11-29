b9272b6fc9242a78a8d8172e9b8697d1f1aa37cf
Will Savoie <wsavoie@gatech.edu>
2016-11-29 14:57:45 -0500

    changed rcm to make it so focal staple is not forced to be in the center

--- scripts/rcm.m
+++ scripts/rcm.m
@@ -32,15 +32,17 @@ vp  = pi*(W)*(D/2)^2+2*pi*L*(D/2)^2+4/3*pi*(D/2)^3;
 
 % vp2 = pi/6*D^3*(3-1) + pi/4*D^2*(W+2*L);
 if L==0
-    pi/6*D^3 + pi/4*D^2*(W+2*L);
+    vp=pi/6*D^3 + pi/4*D^2*(W+2*L);
 end
+% p=(nonCols)/completed;
 p=(completed-nonCols)/completed;
-V=4/3*pi*(dim/2)^3;
+
+V=4/3*pi*((dim)/2)^3;
 
 % phi=[10.8*vp/(p*V) 10.8*vp2/(p*V)]
 phi=C*vp/(p*V);
 % pts('p:',p,'  p*V:', p*V);
-loops=75;
+loops=600;
 % F(loops) = struct('cdata',[],'colormap',[]);
 % close all
 if PON
@@ -55,7 +57,13 @@ if PON
         staples(i).c3=points2(6*(i-1)+5:6*(i-1)+6,:)';
     end
 
-    mainStaple = struct('c1',points1(1:2,:)','c2',points1(3:4,:)','c3',points1(5:6,:)');
+%     mainStaple = struct('c1',points1(1:2,:)','c2',points1(3:4,:)','c3',points1(5:6,:)');
+    mainStaples= struct('c1',zeros(3,2),'c2',zeros(3,2),'c3',zeros(3,2));
+    for (i=1:size(points1,1)/6)
+        mainStaples(i).c1=points1(6*(i-1)+1:6*(i-1)+2,:)';
+        mainStaples(i).c2=points1(6*(i-1)+3:6*(i-1)+4,:)';
+        mainStaples(i).c3=points1(6*(i-1)+5:6*(i-1)+6,:)';
+    end
 
     mainCol = [1,0,0]';
     otherCol= [0,0,1]';
@@ -63,13 +71,18 @@ if PON
 for(i=1:loops)
     hold on;
     plot3(1,1,1);
-    tubeplot(mainStaple.c1,D/2,20,mainCol);
+%     tubeplot(mainStaple.c1,D/2,20,mainCol);
+%     hold on;
+%     
+% %     mesh(sx,sy,sz,'FaceAlpha',.4);
+%     
+%     tubeplot(mainStaple.c2,D/2,20,mainCol);
+%     tubeplot(mainStaple.c3,D/2,20,mainCol);
+    
+    tubeplot(mainStaples(i).c1,D/2,20,mainCol);
     hold on;
-    
-%     mesh(sx,sy,sz,'FaceAlpha',.4);
-    
-    tubeplot(mainStaple.c2,D/2,20,mainCol);
-    tubeplot(mainStaple.c3,D/2,20,mainCol);
+    tubeplot(mainStaples(i).c2,D/2,20,mainCol);
+    tubeplot(mainStaples(i).c3,D/2,20,mainCol);
 
     %plot second staple
     if intersects(i)
