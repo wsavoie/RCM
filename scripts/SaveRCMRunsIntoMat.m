fdd03bac36520af15859c3cd0d32a4bfa2b4f1dd
Will Savoie <wsavoie@gatech.edu>
2016-12-01 14:24:38 -0500

    greatly improved readability of code for reading rcm data

--- scripts/SaveRCMRunsIntoMat.m
+++ scripts/SaveRCMRunsIntoMat.m
@@ -1,34 +1,31 @@
 % directory_name = 'D:\RCMcode\RCM\Results\ushape2\10m 80\';
-directory_name =uigetdir('D:\RCMcode\RCM\Results\');
-multAngles=0;
+fold =uigetdir('D:\RCMcode\RCM\Results\');
+
 C=10;
 PON = [1 1];
-outerFolds = dir2(directory_name);
+outerFolds = dir2(fold);
+outerFolds=outerFolds(find([outerFolds(:).isdir]));
+
 % outerFolds = outerFolds(3:end);
 phiSurf=[];
 phiErr=[];
 figure(5);
 hold on;
+phiSurf=[];
 
 
+for(i=1:length(outerFolds)) 
+    innerFolds=dir2([fold,'/',outerFolds(i).name]);
+    innerFolds=innerFolds(find([innerFolds(:).isdir]));
 
-for(i=1:length(outerFolds))
-    if(multAngles)
-        innerDir = outerFolds(i).name;
-        innerName= [directory_name,'\',innerDir,'\'];
-        innerFolds = dir(innerName);
-        innerFolds = innerFolds(3:end);
-    else
-        innerFolds=outerFolds;
-        innerName= [directory_name,'\'];
+    for j=1:length(innerFolds)
+        innerName=[fold,'\',outerFolds(i).name,'\',innerFolds(j).name,'\'];
+        [lw(j), phi(j), ang1(j), ang2(j)]=rcm(horzcat(innerName),C);
     end
-    for(j=1:length(innerFolds))
-        [lw(j), phi(j), ang1(j), ang2(j)]=rcm(horzcat(innerName,innerFolds(j).name,'\'),C);
-    end
-    temp = [lw' ang1' phi'];
-    temp = sortrows(temp,2);
+
+    temp = [lw' ang1' ang2' phi'];
+    temp = sortrows(temp,[2,3]);
     phiSurf=[phiSurf;temp];
-    
     if PON(2)==2
         
         xlabel('Angle (\circ)');
@@ -41,10 +38,12 @@ for(i=1:length(outerFolds))
             text(OutM(3,1),OutM(end-2,2),horzcat('\itl/w=',num2str(lw(1))))
         end
     end
-    x= errBarCalc(ang1,phi);
-    phiErr = [; lw(1)*ones(size(x,1),1), x(:,1), x(:,2)];
+%     x= errBarCalc(ang1,phi);
+%     phiErr = [; lw(1)*ones(size(x,1),1), x(:,1), x(:,2)];
+save([fold,'/',outerFolds(i).name,'/','phiLW.mat'],'phiSurf');
 end
-save('phiErr.mat')
+% save('phiErr.mat')
+
 figText(gcf,14);
 figure(45);
 hold on;
